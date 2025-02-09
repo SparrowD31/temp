5675542f8f50cb62b646f0621a02c466fde45764
AvinashD31 <avi31032002@gmail.com>
2025-02-09 14:05:58 +0530

    "userRoutes update"

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -5,9 +5,8 @@ const Order = require('../models/Order');
 const mongoose = require('mongoose');
 
 // Get user by ID
-router.get('/users/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
-    
     const user = await User.findById(req.params.id).select('-password'); // Exclude password
     
     if (!user) {
@@ -22,7 +21,7 @@ router.get('/users/:id', async (req, res) => {
 });
 
 // Update user
-router.put('/users/:id', async (req, res) => {
+router.put('/:id', async (req, res) => {
   try {
     const { name, email, mobile } = req.body;
     
@@ -56,7 +55,7 @@ router.get('/orders', async (req, res) => {
 });
 
 // Update user address and mobile
-router.put('/users/:userId/address', async (req, res) => {
+router.put('/:userId/address', async (req, res) => {
   try {
     const { userId } = req.params;
     const { address, mobile } = req.body;
